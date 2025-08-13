77b6a1de515bd952b077e6589d4ae669a3d555d4
kurogami20 <dam31vt@gmail.com>
2025-08-13 10:39:10 +0200

    adhérer est responsive

--- views/adherer.php
+++ views/adherer.php
@@ -1,23 +1,23 @@
 <section class="flex flex-col p-4 min-[1480px]:px-40 min-[1200px]:px-20 px-2 h-[100%] gap-20 items-center">
     <h2 class="text-5xl text-center capitalize font-bold ">Adhérer</h2>
     <section class=" p-10 flex flex-col items-center justify-center gap-10 border border-[#ffbe46] rounded-lg shadow-sm   bg-white  ">
-        <p class="text-lg text-center ">
+        <p class="text-base sm:text-lg text-center ">
         Adhérer à l’Association AILE, c’est bien plus qu’une simple inscription. C’est rejoindre une communauté dynamique portée par des bénévoles engagés, et bénéficier d’un accès à de nombreuses activités culturelles, sportives et conviviales tout au long de l’année.
         </p>
     </section>
-    <section class=" grid-cols-2 grid gap-10">
+    <section class="grid-cols-1 min-[1000px]:grid-cols-2 grid gap-10">
         <article class="card border border-[#ffbe46] rounded-lg shadow-sm bg-white p-10 flex flex-col gap-3">
-            <h2 class="title text-2xl font-bold">Dates des permanences</h2>
+            <h1 class="title text-xl sm:text-2xl font-bold">Dates des permanences</h1>
             <div class="card-content">
                 <p><strong>Journées d'inscriptions :</strong></p>
-                <ul class="flex flex-col gap-2 py-1">
+                <ul class="text-base sm:text-lg flex flex-col gap-2 py-1">
                     <li>📅 <strong>Jeudi 03 juillet</strong> de 9h à 16h – Salle de la Convivialité à Cugnaux</li>
                     <li>📅 <strong>Vendredi 29 août</strong> de 9h à 16h – Salle de la Convivialité à Cugnaux</li>
                 </ul>
                 <p><strong>Uniquement pour les nouvelles adhésions :</strong></p>
-                <ul class="flex flex-col gap-2 py-1">
+                <ul class="text-base sm:text-lg flex flex-col gap-2 py-1">
                     <li>📍 Forum des associations :</li>
-                    <ul class="flex flex-col gap-2 py-1">
+                    <ul class="text-base sm:text-lg flex flex-col gap-2 py-1">
                         <li>Samedi 6 septembre après-midi – Frouzins</li>
                         <li>Samedi 6 septembre après-midi – Villeneuve-Tolosane</li>
                         <li>Dimanche 7 septembre de 9h à 13h – Cugnaux</li>
@@ -27,9 +27,9 @@
             </div>
         </article>
         <article class="card border border-[#ffbe46] rounded-lg shadow-sm bg-white p-10 flex flex-col gap-3">
-            <h2 class="title text-2xl font-bold">Documents à fournir</h2>
+            <h1 class="title text-xl sm:text-2xl font-bold">Documents à fournir</h1>
             <div class="card-content">
-                <ul class="flex flex-col gap-2 py-1">
+                <ul class="text-base sm:text-lg flex flex-col gap-2 py-1">
                     <li>📝 Demande d'inscription remplie et signée</li>
                     <li>🖼️ Photo d'identité de moins de 3 ans (pas de photocopie)</li>
                     <li>✉️ Enveloppe timbrée libellée à votre nom et adresse</li>
@@ -39,19 +39,19 @@
             </div>
         </article>
         <article class="card border border-[#ffbe46] rounded-lg shadow-sm bg-white p-10 flex flex-col gap-3">
-            <h2 class="title text-2xl font-bold">Adhésion pour activités sportives</h2>
+            <h1 class="title text-xl sm:text-2xl font-bold">Adhésion pour activités sportives</h1>
             <div class="card-content">
                 <p>Pour les séjours sportifs (raquettes, ski de fond, vélo, SMS), une adhésion FFRS est obligatoire :</p>
-                <ul class="flex flex-col gap-2 py-1">
+                <ul class="text-base sm:text-lg flex flex-col gap-2 py-1">
                     <li>💳 38,21 € (par chèque à l'ordre de AILE)</li>
                 </ul>
                 <p>L’association vous invite à consulter les <strong>Recommandations Santé</strong> à conserver pour toute activité sportive.</p>
             </div>
         </article>
         <article class="card border border-[#ffbe46] rounded-lg shadow-sm bg-white p-10 flex flex-col gap-3">
-            <h2 class="title text-2xl font-bold">Cotisation annuelle</h2>
+            <h1 class="title text-xl sm:text-2xl font-bold">Cotisation annuelle</h1>
             <div class="card-content">
-                <ul class="flex flex-col gap-2 py-1">
+                <ul class="text-base sm:text-lg flex flex-col gap-2 py-1">
                     <li>👤 24 € par personne</li>
                     <li>👥 48 € pour un couple</li>
                     <li>📰 9 € en plus pour recevoir le journal par courrier</li>
@@ -63,11 +63,11 @@
             </div>
         </article>
         <article class="card border border-[#ffbe46] rounded-lg shadow-sm bg-white p-10 flex flex-col gap-3">
-            <h2 class="title text-2xl font-bold">Validation de l'adhésion</h2>
+            <h1 class="title text-xl sm:text-2xl font-bold">Validation de l'adhésion</h1>
             <div class="card-content">
                 <p>Chaque demande sera examinée par le Conseil d’Administration.</p>
                 <p>La décision dépendra :</p>
-                <ul class="flex flex-col gap-2 py-1">
+                <ul class="text-base sm:text-lg flex flex-col gap-2 py-1">
                     <li>✅ De la disponibilité dans les activités demandées</li>
                     <li>✅ Des règlements en vigueur</li>
                 </ul>
@@ -75,9 +75,9 @@
             </div>
         </article>
         <article class="card border border-[#ffbe46] rounded-lg shadow-sm bg-white p-10 flex flex-col gap-3">
-            <h2 class="title text-2xl font-bold">Document à télécharger</h2>
+            <h1 class="title text-xl sm:text-2xl font-bold">Document à télécharger</h1>
             <div class="card-content">
-                <ul class="flex flex-col gap-2 py-1">
+                <ul class="text-base sm:text-lg flex flex-col gap-2 py-1">
                     <li>⬇️ <a class="text-[#A46A00] underline " href="/assets/documents/demande_inscription_2025-2026.pdf" target="_blank" >Demande d'inscription 2025/2026</a></li>
                     <li>⬇️ <a class="text-[#A46A00] underline " href="/assets/documents/recommandations-sante-2023.pdf" target="_blank" >Recommandations santé</a></li>
                     <li>⬇️ <a class="text-[#A46A00] underline " href="/assets/documents/STATUTS2025.pdf" target="_blank" >Status 2025</a></li>
@@ -91,10 +91,10 @@
     </section>
     <section class=" ">
         <article class="card border border-[#ffbe46] rounded-lg shadow-sm bg-white p-10 flex flex-col gap-3 items-center justify-center">
-                <h2 class="title text-2xl font-bold">Contact</h2>
+                <h1 class="title text-xl sm:text-2xl font-bold">Contact</h1>
                 <div class="card-content flex flex-col items-center gap-3">
                     <p class="text-lg text-center">N'hésitez pas à nous contacter si vous avez la moindre question</p>
-                    <ul class="flex flex-col gap-2 py-1 items-center">
+                    <ul class="text-base sm:text-lg flex flex-col gap-2 py-1 items-center">
                         <li>📧 <a href="mailto:contact.association@aile31.fr" class="text-[#A46A00] underline ">contact.association@aile31.fr</a></li>
                         <li>📞 <a href="tel:+33972300825" class="text-[#A46A00] underline ">09 72 30 08 25 ( serveur vocal de l'association )</a></li>
                         <li>📍 <a href="https://maps.app.goo.gl/5GzDbwUjsQWuGX9p6" target="_blank" class="text-[#A46A00] underline ">Association AILE 4 rue Hélène Boucher 31270, Cugnaux</a></li>
